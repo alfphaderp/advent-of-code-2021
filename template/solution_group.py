ca70ba47de4995bde84619db1c83739ad7e97bb8
Ryan Zhu <ryanzhu@berkeley.edu>
2021-12-03 22:36:47 -0800

    Rename crap_solution.py to dirty_solution.py

--- template/solution_group.py
+++ template/solution_group.py
@@ -8,17 +8,19 @@ def group(lines):
     groups, group = [], []
     for line in lines:
         if line != "":
-            group.extend(line.split(" "))
+            group.append(line)
         else:
             groups.append(group)
             group = []
     groups.append(group)
     return groups
 
+import copy
+
 if __name__ == '__main__':
     file = 'input.txt'
     # file = 'input_sample.txt'
     with open(file, 'r') as f:
         groups = group(f.read().splitlines())
-    print(part1(groups))
-    print(part2(groups))
+    print(part1(copy.deepcopy(groups)))
+    print(part2(copy.deepcopy(groups)))
