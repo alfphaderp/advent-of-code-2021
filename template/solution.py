5be46bb5094d5d795a2d55645ebb97fe503cbfc1
Ryan Zhu <ryanzhu@berkeley.edu>
2021-12-03 22:38:02 -0800

    Update template to deep copy inputs

--- template/solution.py
+++ template/solution.py
@@ -4,11 +4,13 @@ def part1(lines):
 def part2(lines):
     pass
 
+import copy
+
 if __name__ == '__main__':
     file = 'input.txt'
     # file = 'input_sample.txt'
     with open(file, 'r') as f:
         lines = f.read().splitlines()
     # lines = [int(i) for i in lines]
-    print(part1(lines))
-    print(part2(lines))
+    print(part1(copy.deepcopy(lines)))
+    print(part2(copy.deepcopy(lines)))
