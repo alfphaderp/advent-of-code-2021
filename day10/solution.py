ff981e801b7d6bbcac0cb85af94ab433f555206a
Ryan Zhu <ryanzhu@berkeley.edu>
2021-12-10 22:25:31 -0800

    Clean up day 10

--- day10/solution.py
+++ day10/solution.py
@@ -1,53 +1,38 @@
-p = {
-    '}': '{',
-    ']': '[',
-    ')': '(',
-    '>': '<'
-}
-
-pts = {
-    '}': 1197,
-    ']': 57,
-    ')': 3,
-    '>': 25137
-}
-
-inc = []
-
-def illegal(l):            
-    s = []
-    for c in l:
-        if c in '{[(<':
-            s.append(c)
-        else:
-            if not s or s[-1] != p[c]:
-                return pts[c]
-            else:
-                s.pop(-1)
-    inc.append(s)
-    return 0
-    
 def part1(lines):
-    return sum(illegal(l) for l in lines)
-    
-pts2 = {
-    '(': 1,
-    '[': 2,
-    '{': 3,
-    '<': 4
-}
-    
+    points = {')': 3, ']': 57, '}': 1197, '>': 25137}
+    total = 0
+    for line in lines:
+        state, symbol = verify(line)
+        if state == 'CORRUPTED':
+            total += points[symbol]
+    return total
+
 def part2(lines):
-    a = []
-    for s in inc:
-        s = s[::-1]
-        sc = 0
-        for p in s:
-            sc *= 5
-            sc += pts2[p]
-        a.append(sc)
-    a.sort()
-    return a[len(a) // 2]
+    points = {'(': 1, '[': 2, '{': 3, '<': 4}
+    scores = []
+    for line in lines:
+        state, stack = verify(line)
+        if state == 'INCOMPLETE':
+            score = 0
+            for symbol in stack[::-1]:
+                score *= 5
+                score += points[symbol]
+            scores.append(score)
+    scores.sort()
+    return scores[len(scores) // 2]
+
+def verify(line):
+    opening = {'}': '{', ']': '[', ')': '(', '>': '<'}
+    stack = []
+    for symbol in line:
+        if symbol in '{[(<':
+            stack.append(symbol)
+        else:
+            if not stack or stack[-1] != opening[symbol]:
+                return 'CORRUPTED', symbol
+            else:
+                stack.pop(-1)
+    return 'INCOMPLETE', stack
 
 import copy
 
@@ -56,6 +41,5 @@ if __name__ == '__main__':
     # file = 'input_sample.txt'
     with open(file, 'r') as f:
         lines = f.read().splitlines()
-    # lines = [int(i) for i in lines]
     print(part1(copy.deepcopy(lines)))
     print(part2(copy.deepcopy(lines)))
