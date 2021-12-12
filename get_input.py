c5fcd732c81e6a61efe79da0c03b67410732dbee
Ryan Zhu <ryanzhu@berkeley.edu>
2021-12-12 00:19:26 -0800

    Add some error handling to get_input.py

--- get_input.py
+++ get_input.py
@@ -10,30 +10,33 @@ if not Path('./session').is_file():
 
 args = sys.argv
 if len(args) != 2:
-    print(f'Error: incorrect number of arguments')
+    print(f'Error: incorrect number of arguments!')
     exit()
 if not args[1].isdigit():
-    print(f'Error: {sys.argv[0]} is not a number')
+    print(f'Error: {sys.argv[0]} is not a number!')
     exit()
 day = int(args[1])
 if not 1 <= day <= 25:
-    print(f'Error: {day} is not a valid day')
+    print(f'Error: {day} is not a valid day!')
     exit()
 if not Path(f'./day{day}/').is_dir():
-    print(f'Error: input file for day {day} has not been created')
+    print(f'Error: input file for day {day} has not been created!')
     exit()
 
 with open('./session', 'r') as f:
     session = f.read()
     f.close()
 
-url = f'https://adventofcode.com/2021/day/{day}/input'
-headers = {'Cookie': f'session={session}'}
-request = Request(url, headers=headers)
-puzzle_input = urlopen(request).read().decode('ascii')
+try:
+    url = f'https://adventofcode.com/2021/day/{day}/input'
+    headers = {'Cookie': f'session={session}'}
+    request = Request(url, headers=headers)
+    puzzle_input = urlopen(request).read().decode('ascii')
 
-with open(f'./day{day}/input.txt', 'w') as f:
-    f.write(puzzle_input)
-    f.close()
-
-print(f'Successfully retrieved puzzle input for day {day}')
+    with open(f'./day{day}/input.txt', 'w') as f:
+        f.write(puzzle_input)
+        f.close()
+    print(f'Successfully retrieved puzzle input for day {day}!')
+except Exception as e:
+    print(f'Unsuccessful in retrieving puzzle input for day {day}:')
+    print(e)
